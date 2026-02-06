8cc41786a32765cb5624b2bec3d790c0dc21008f
frank zaraki <frankndounga311@gmail.com>
2026-02-06 17:38:01 +0100

    initialize repo with the ceasar_cypher

---
+++ README.md
@@ -0,0 +1,4 @@
+# SOME PYTHON MODULES WHEN I AM TRAINING
+
+## AN IMPROVED VERSION OF THE CEASAR_CYPHER ALOGORITHM 
+I have just added the functionnality to not ignore non alpha letter and let them as they are in the script and also add lowercase letter into the game, a shifted value can also be specified into the function and default to 1 the available range of the shifted value is set into the function and i have implemented a decrypted function to test the script.
---
+++ improved_ceasar_cypher.py
@@ -0,0 +1,43 @@
+def ceasar_cypher(message, shifted=1):
+    MAX_SHIFT = 20
+    if shifted < 1 or shifted > MAX_SHIFT:
+        print(f'the shifted value cannot be less than 1 or greater tha {MAX_SHIFT} it default to 1.')
+        shifted = 1
+    encrypted_message = ''
+    for letter in message:
+        if letter.isalpha():
+            code = ord(letter) + shifted
+            if letter.isupper() and code >= ord('Z'):
+                code = ord('A')
+            if letter.islower() and code >= ord('z'):
+                code = ord('a')
+            encrypted_message += chr(code)
+        else:
+            encrypted_message += letter
+    return encrypted_message
+
+def decrypt_ceasar_cypher(encrypted_message, shifted=1):
+    MAX_SHIFT = 20
+    if shifted < 1 or shifted > 20:
+        print(f'the shifted value cannot be less than 1 or greater tha {MAX_SHIFT} it default to 1.')
+        shifted = 1
+    decrypted_message = ''
+    for letter in encrypted_message:
+        if not letter.isalpha():
+            decrypted_message += letter
+            continue
+        code = ord(letter) - shifted
+        if letter.isupper() and code < ord('A'):
+            code = ord('Z')
+        if letter.islower() and code < ord('a'):
+            code = ord('z')
+        decrypted_message += chr(code)
+    return decrypted_message
+
+if __name__ == '__main__':
+    shifted_number = 4
+    message = input('Enter the message you want to encrypted: ')
+    encrypted_message = ceasar_cypher(message, shifted_number)
+    print('The encrypted message is: ', encrypted_message)
+    decrypted_message = decrypt_ceasar_cypher(encrypted_message, shifted_number)
+    print('The decrypted message is: ', decrypted_message)
